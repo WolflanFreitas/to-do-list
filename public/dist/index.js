756bc4925cf7274fa147f47eb93885dbc375b8e2
Wolflan Freitas <wolflanf@gmail.com>
2023-07-27 15:07:56 -0300

    Interfaces

--- public/dist/index.js
+++ public/dist/index.js
@@ -1,13 +1,33 @@
 "use strict";
 (function () {
-    var todo = {
-        description: 'todo',
-        done: false
-    };
-    var reminder = {
-        description: 'reminder',
-        date: '2023-07-27'
-    };
+    var Reminder = /** @class */ (function () {
+        function Reminder(description, date, notifications) {
+            this.id = '';
+            this.created_at = new Date();
+            this.updated_at = new Date();
+            this.description = description;
+            this.date = date;
+            this.notifications = notifications;
+        }
+        Reminder.prototype.render = function () {
+            return JSON.stringify(this);
+        };
+        return Reminder;
+    }());
+    var Todo = /** @class */ (function () {
+        function Todo(description) {
+            this.id = '';
+            this.created_at = new Date();
+            this.updated_at = new Date();
+            this.description = description;
+        }
+        Todo.prototype.render = function () {
+            return JSON.stringify(this);
+        };
+        return Todo;
+    }());
+    var todo = new Todo('TODO criado com a classe');
+    var reminder = new Reminder('REMINDER criado com a classe', new Date('2023-07-27'), ['EMAIL']);
     var taskView = {
         render: function (tasks) {
             var tasksList = document.querySelector('#tasksList');
@@ -16,7 +36,7 @@
             }
             tasks.forEach(function (task) {
                 var li = document.createElement('li');
-                var textNode = document.createTextNode(JSON.stringify(task));
+                var textNode = document.createTextNode(task.render());
                 li.appendChild(textNode);
                 tasksList === null || tasksList === void 0 ? void 0 : tasksList.appendChild(li);
             });
--- src/index.ts
+++ src/index.ts
@@ -1,16 +1,53 @@
 (() => {
-    const todo = {
-        description: 'todo',
-        done: false
-    };
+    interface Task {
+        id: string;
+        created_at: Date;
+        updated_at: Date;
+        description: string;
+        render(): string;
+    }
 
-    const reminder = {
-        description: 'reminder',
-        date: '2023-07-27'
-    };
+    class Reminder implements Task {
+        id: string = '';
+        created_at: Date = new Date();
+        updated_at: Date = new Date();
+        description: string;
+
+        date: Date;
+        notifications: Array<string>;
+
+        constructor(description: string, date: Date, notifications: Array<string>) {
+            this.description = description;
+            this.date = date;
+            this.notifications = notifications;
+        }
+
+        render(): string {
+            return JSON.stringify(this);
+        }
+    }
+
+    class Todo implements Task {
+        id: string = '';
+        created_at: Date = new Date();
+        updated_at: Date = new Date();
+        description: string;
+
+        constructor(description: string) {
+            this.description = description;
+        }
+
+        render(): string {
+            return JSON.stringify(this);
+        }
+    }
+
+    const todo = new Todo('TODO criado com a classe');
+
+    const reminder = new Reminder('REMINDER criado com a classe',new Date('2023-07-27'),['EMAIL'])
 
     const taskView = {
-        render(tasks: Array<Object>) {
+        render(tasks: Array<Task>) {
             const tasksList = document.querySelector('#tasksList');
 
             while(tasksList?.firstChild) {
@@ -19,7 +56,7 @@
 
             tasks.forEach((task) => {
                 const li = document.createElement('li');
-                const textNode = document.createTextNode(JSON.stringify(task));
+                const textNode = document.createTextNode(task.render());
                 li.appendChild(textNode);
                 tasksList?.appendChild(li);
             });
@@ -27,7 +64,7 @@
     }
 
     const TaskController = (view: typeof taskView) => {
-        const tasks: Array<Object> = [todo,reminder];
+        const tasks: Array<Task> = [todo,reminder];
 
         const eventHandler = (event: Event) => {
             event.preventDefault();
